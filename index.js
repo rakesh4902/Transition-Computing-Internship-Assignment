0a425394fbec8b9f8f3be5bb6e75bf1ab521394d
rakesh4902 <modir810@gmail.com>
2024-04-10 00:05:50 +0530

    solution

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/UserManagementDB')
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('Error connecting to MongoDB:', err));
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Error connecting to MongoDB:', err));
 
 // Middleware
 app.use(express.json());
